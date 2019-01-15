b30c07c54b619529cc2b1898d0d1210180fcd67c
Liqiao Ying <benhero@gmail.com>
2019-01-15 10:41:42 -0500

    fixed arguments

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -29,11 +29,11 @@ contract SimpleBank {
     event LogEnrolled(address account);
 
     /* Add 2 arguments for this event, an accountAddress and an amount */
-    event LogDepositMade(address account, uint amount);
+    event LogDepositMade(address accountAddress, uint amount);
 
     /* Create an event called LogWithdrawal */
     /* Add 3 arguments for this event, an accountAddress, withdrawAmount and a newBalance */
-    event LogWithdrawal(address account, uint withdrawAmount, uint newBalance);
+    event LogWithdrawal(address accountAddress, uint withdrawAmount, uint newBalance);
 
     //
     // Functions
